fa8e4b4caf354307c6f8e1a92eb82890b458a051
张亚飞 <672233580@qq.com>
2021-09-08 14:23:25 +0800

    Initial Commit

---
+++ ScrollViewRefreshable0908/ScrollViewRefreshable0908App.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollViewRefreshable0908App.swift
+//  ScrollViewRefreshable0908
+//
+//  Created by 张亚飞 on 2021/9/8.
+//
+
+import SwiftUI
+
+@main
+struct ScrollViewRefreshable0908App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
